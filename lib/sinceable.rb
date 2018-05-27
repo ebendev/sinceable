a44bc16f3cc8980bef87c80f701cadb64eaba409
Eben Geer <ebengeer@gmail.com>
2018-05-26 20:31:19 -0500

    initial release

--- lib/sinceable.rb
+++ lib/sinceable.rb
@@ -1,5 +1,12 @@
 require "sinceable/version"
 
 module Sinceable
-  # Your code goes here...
+  extend ActiveSupport::Concern
+
+  included do
+    scope :since, ->(time, column = :created_at) { where(arel_table[column].gteq(time)) }
+    scope :until, ->(time, column = :created_at) { where(arel_table[column].lt(time)) }
+  end
 end
+
+ActiveRecord::Base.send :include, Sinceable
